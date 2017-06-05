3069cc618bd41100bac7f03e2574e01fbe3c804d
iamnotlee <438068183@qq.com>
2017-06-05 23:32:09 +0800

    MahJongModel

--- Assets/Scripts/Model/MahJongModel.cs
+++ Assets/Scripts/Model/MahJongModel.cs
@@ -6,7 +6,19 @@ public class MahJongModel : Singleton<MahJongModel> {
 	
     public void RegistCallBack()
     {
+        //CDataListManager.instance.RegisterDelegate<RPCreateRoom>(CreateRoom);
 
+    }
+    void CreateRoom(PB_BaseData baseData)
+    {
+        //RPCreateRoom rp = baseData.GetObj<RPCreateRoom>();
+        //if (rp != null)
+        //{
+        //    //28682766
+        //    UiManager.Instance.CloseUi(EnumUiType.Room_CreateRoomUI);
+        //    UnityEngine.SceneManagement.SceneManager.LoadScene("game");
+        //    Debug.Log(rp.roomId);
+        //}
     }
 
 }
